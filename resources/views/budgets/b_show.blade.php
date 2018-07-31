5c549f21be0f5794f6be2fda1bce36bffe24e87b
Severin-Obrist <akennef@gmail.com>
2018-07-31 12:20:14 +0200

    tried to fix b_show

--- resources/views/budgets/b_show.blade.php
+++ resources/views/budgets/b_show.blade.php
@@ -1,18 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+    <?php 
+    $budgetArray = [];
+    ?>
     @if(count($budget) > 0)
         <table class='table table-striped mt-2'>
             <tr>
                 <th>Budgetposten</th>
                 <th>Eintrag</th>
             </tr>
-            @foreach($budget as $eintrag)
-                <tr>
-                    <td>{{$eintrag->budgetPosten}}</td>
-                    <td>{{$eintrag->content}}</td>
-                </tr>
+            @foreach($budget as $posten => $wert) <!-- iteriert durch die verschiedenen budgetposten in einem budget -->
+                <?php 
+                echo $wert->budgetPosten.": ".$wert->content; 
+                echo "</br>";
+                echo $wert;
+                echo "</br>";
+                echo $posten;
+                echo "</br>";
+                if(array_key_exists($wert->budgetPosten, $budgetArray)){
+                    echo $wert->budgetPosten." ist im array";
+                }else{
+                    echo $wert->budgetposten." ist nicht im array";
+                    array_push($budgetArray, $wert);
+                }
+                /*
+                foreach($budgetArray as $eintrag){
+                    echo $eintrag;
+                }*/
+                echo "</br>";
+                ?>
             @endforeach
+                
         </table>
     @else
         <p>Keine Einträge im Buget</p>
